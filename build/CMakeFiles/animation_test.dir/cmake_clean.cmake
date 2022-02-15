4b2921c8df7c37881f84daa28d360668479eaf8d
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2022-02-16 02:11:09 +0900

    add gnuplot animation

---
+++ build/CMakeFiles/animation_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/animation_test.dir/src/animation_test.cpp.o"
+  "animation_test.pdb"
+  "animation_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/animation_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/animation_test.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <cmath>
+
+void writeFile(std::string fname, double time){
+    static bool isFirst = true;
+    FILE *fp;
+    if(isFirst == true){
+        fp = fopen(fname.c_str(), "w");
+    }else{
+        fp = fopen(fname.c_str(), "a");
+    }
+
+    double y1 = sin(time);
+    double y2 = cos(time);
+    fprintf(fp, "%lf %lf %lf\n", time, y1, y2);
+    fclose(fp);
+
+    isFirst = false;
+
+}
+
+void animationPlot(std::string fname, double time){
+    static FILE *gp;
+    if(gp == NULL){
+        gp = popen("gnuplot -persist", "w");
+        fprintf(gp, "set yrange [-1.0:1.0]\n");
+    }
+    fprintf(gp, "set xrange [%lf:%lf]\n", time - 5.0, time + 5.0);
+
+    fprintf(gp, "plot \"%s\" u 1:2 w l t\"sin\",  \"%s\" u 1:3 w l t\"cos\"\n", fname.c_str(), fname.c_str());
+    fflush(gp);
+}
+
+int main(int argc, char **argv){
+    std::string fname = "animation_data.txt";
+    double time = 0.0;
+    for (; ;){
+        writeFile(fname, time);
+        animationPlot(fname, time);
+        time += 0.1;
+        usleep(100000);
+
+    }
+    return 0;
+}
